72c7de45c7e62cfc6d076fff2e359680d3cfa353
Aestebanm18 <aestebanmdlc@gmail.com>
2024-04-15 14:44:01 +0200

    Metodo existe() documentado

--- src/FUtil.java
+++ src/FUtil.java
@@ -1,6 +1,15 @@
 
 import java.io.File;
 public class FUtil {
+
+    /**
+     * Este método verifica si un archivo existe en el sistema de archivos.
+     *
+     * @param filename El nombre del archivo que se va a verificar. No puede ser una cadena vacía.
+     * @return true si el archivo existe, false en caso contrario.
+     * @throws IllegalArgumentException si el nombre del archivo es una cadena vacía.
+     */
+
     public static boolean existe(String filename) throws IllegalArgumentException{
         if(filename.isBlank()){
             throw new IllegalArgumentException("Cadena vacia");
