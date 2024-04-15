dc30df6f8cf7a8602ab7bd59e05d060fe41e6f1e
Aestebanm18 <aestebanmdlc@gmail.com>
2024-04-15 14:35:26 +0200

    Error cadena vacia solucionado

--- src/FUtil.java
+++ src/FUtil.java
@@ -1,7 +1,10 @@
 
 import java.io.File;
 public class FUtil {
-    public static boolean existe(String filename) {
+    public static boolean existe(String filename) throws IllegalArgumentException{
+        if(filename.isBlank()){
+            throw new IllegalArgumentException("Cadena vacia");
+        }
         File file = new File(filename);
         if (file.exists()) {
             return true;
